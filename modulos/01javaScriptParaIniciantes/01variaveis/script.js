cf0142565e5607922482639fd217efd27d889e1b
Brenddon Andrade <brenddonandrade@gmail.com>
2025-01-07 17:26:17 -0300

    :package: feito a parte um sobre variáveis

---
+++ modulos/01javaScriptParaIniciantes/01variaveis/script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Brenddon';
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+var idade = 26;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var pudim;
+console.log(pudim);
+
+// Atribuir valor a sua comida favorita
+pudim = 'gostoso';
+console.log(pudim);
+
+// Declarar 5 variáveis diferentes sem valores
+var v1,v2,v3,v4,v5;
+console.log(v1,v2,v3,v4,v5);
