6398d29876a2c1af3c9e879ede286b1330560a95
Brenddon Andrade <brenddonandrade@gmail.com>
2025-01-08 09:58:29 -0300

    :up: enviando o trabalho sobre o tipo de dado js

---
+++ modulos/01javaScriptParaIniciantes/02tiposDeDados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nome = 'Brenddon';
+console.log(nome);
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = 26;
+var texto = `${nome} tem ${idade} anos.`;
+console.log(texto);
+
+// Declare uma variável com a sua idade
+var idade = 26;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Érick Andrade de Oliveira';
+console.log(nome + ' ' + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var texto2 = "It's time";
+console.log(texto2);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
